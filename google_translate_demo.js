d93d9139cfdcc43c0ebd7b76c44c813b8f7c73cd
RajKKapadia <raajforyou@gmail>
2020-10-05 13:01:41 +0530

    first commit

---
+++ google_translate_demo.js
@@ -0,0 +1,49 @@
+const {Translate} = require('@google-cloud/translate').v2;
+require('dotenv').config();
+
+// Your credentials
+const CREDENTIALS = JSON.parse(process.env.CREDENTIALS);
+
+// Configuration for the client
+const translate = new Translate({
+    credentials: CREDENTIALS,
+    projectId: CREDENTIALS.project_id
+});
+
+const detectLanguage = async (text) => {
+
+    try {
+        let response = await translate.detect(text);
+        return response[0].language;
+    } catch (error) {
+        console.log(`Error at detectLanguage --> ${error}`);
+        return 0;
+    }
+}
+
+// detectLanguage('Oggi è lunedì')
+//     .then((res) => {
+//         console.log(res);
+//     })
+//     .catch((err) => {
+//         console.log(error);
+//     });
+
+const translateText = async (text, targetLanguage) => {
+
+    try {
+        let [response] = await translate.translate(text, targetLanguage);
+        return response;
+    } catch (error) {
+        console.log(`Error at translateText --> ${error}`);
+        return 0;
+    }
+};
+
+// translateText('Oggi è lunedì', 'en')
+//     .then((res) => {
+//         console.log(res);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
